9a6d562e28cc57038773beb41e68019609c81f6b
pushkarguptaaa <pushkardps8@gmail.com>
2022-07-09 18:50:05 +0530

    Created game history

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Board from './components/Board';
+import History from './components/History';
 import { calculateWinner } from './helpers';
 import './styles/styles.scss';
 
@@ -10,7 +11,6 @@ const App = () => {
   const [currentMove, setCurrentMove] = useState(0);
   const current = history[currentMove];
 
-  console.log('history:', history);
   const winner = calculateWinner(current.board);
 
   const message = winner
@@ -31,12 +31,16 @@ const App = () => {
     });
     setCurrentMove(prev => prev + 1);
   };
+  const moveTo = move => {
+    setCurrentMove(move);
+  };
 
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <h2>{message}</h2>
       <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 };
