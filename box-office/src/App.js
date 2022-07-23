3d747147a6b5f8e628df6358245fc27ab04738aa
pushkarguptaaa <pushkardps8@gmail.com>
2022-07-23 14:20:33 +0530

    Created theme for styled components and a few styled components

--- box-office/src/App.js
+++ box-office/src/App.js
@@ -1,30 +1,41 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import Home from './pages/Home';
 import Show from './pages/Show';
 import Starred from './pages/Starred';
 
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
 function App() {
   return (
-    <div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
+    <ThemeProvider theme={theme}>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
 
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
+          <Route exact path="/starred">
+            <Starred />
+          </Route>
 
-        <Route exact path="/show/:id">
-          <Show />
-        </Route>
+          <Route exact path="/show/:id">
+            <Show />
+          </Route>
 
-        <Route>
-          <div>Not found!</div>
-        </Route>
-      </Switch>
-    </div>
+          <Route>
+            <div>Not found!</div>
+          </Route>
+        </Switch>
+      </div>
+    </ThemeProvider>
   );
 }
 
