a8af87fb43a4455af5c7dce4f65736b804abb9b9
pushkarguptaaa <pushkardps8@gmail.com>
2022-07-27 00:55:52 +0530

    Initialised firebase project

---
+++ chat-app/src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyAHOswR3rve6ZvlZ6kJDrc4O2okUkXdJCo',
+  authDomain: 'chat-web-app-e9d19.firebaseapp.com',
+  projectId: 'chat-web-app-e9d19',
+  storageBucket: 'chat-web-app-e9d19.appspot.com',
+  messagingSenderId: '406629961387',
+  appId: '1:406629961387:web:dbef6295352cbf53d6b1ee',
+};
+
+const app = firebase.initializeApp(config);
