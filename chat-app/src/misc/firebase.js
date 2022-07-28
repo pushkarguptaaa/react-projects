0cb162503eb57189eaefee3ac09dc5ae098aecf2
pushkarguptaaa <pushkardps8@gmail.com>
2022-07-28 14:46:08 +0530

    Created public and private routes

--- chat-app/src/misc/firebase.js
+++ chat-app/src/misc/firebase.js
@@ -7,6 +7,8 @@ const config = {
   storageBucket: 'chat-web-app-e9d19.appspot.com',
   messagingSenderId: '406629961387',
   appId: '1:406629961387:web:dbef6295352cbf53d6b1ee',
+  databaseURL:
+    'https://chat-web-app-e9d19-default-rtdb.asia-southeast1.firebasedatabase.app/',
 };
 
 const app = firebase.initializeApp(config);
